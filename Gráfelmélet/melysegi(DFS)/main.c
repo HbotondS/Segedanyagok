9453d3fe701816c058bfea7b7d73effef1ee2022
Botond Sámuel Hegyi <36891185+HbotondS@users.noreply.github.com>
2018-08-27 12:13:50 +0300

    Graph: update BFS & DFS

--- Gráfelmélet/melysegi(DFS)/main.c
+++ Gráfelmélet/melysegi(DFS)/main.c
@@ -1,24 +1,26 @@
+// megallapitja az elek tipusat
+
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Node{
+typedef struct Node {
     int degreeIn;
     int degreeOut;
-    int* neightIn; ///a csomopont szomszedjai
+    int* neightIn; // a csomopont szomszedjai
     int* neightOut;
-    int color; ///0 = feher(meg nem jartunk ott); 1 = szurke(most vagyunk ott)\
+    int color; // 0 = feher(meg nem jartunk ott); 1 = szurke(most vagyunk ott)\
                     2 = fekete(mar jartunk ott)
-    int reach; /// mikor erjuk el, arra kell hogy megtudjuk hogy allapitani az eleket
+    int reach; // mikor erjuk el, arra kell hogy megtudjuk hogy allapitani az eleket
     int parent;
     int leave;
 }Node;
 
-void nodeOut(Node* t, int n){
+void nodeOut(Node* t, int n) {
     int i, j;
-    for(i = 1; i <= n; ++i){
+    for(i = 1; i <= n; ++i) {
         printf("%d: ", i);
         printf("Out: ");
-        for(j = 0; j < t[i].degreeOut; ++j){
+        for(j = 0; j < t[i].degreeOut; ++j) {
             printf("%d ", t[i].neightOut[j]);
         }
         printf("Kifokszam: %d", t[i].degreeOut);
@@ -26,37 +28,37 @@ void nodeOut(Node* t, int n){
     }
 }
 
-void timeOut(Node* t, int n){
+void timeOut(Node* t, int n) {
     int i;
-    for(i = 1; i <= n; ++i){
+    for(i = 1; i <= n; ++i) {
         printf("%d: %d\n", i, t[i].reach);
     }
 }
 
-void depth(Node* t, int i, int time){
-    ///printf("%d ", i);
+void depth(Node* t, int i, int time) {
+    // printf("%d ", i);
     t[i].color = 1;
     ++time;
     t[i].reach = time;
     int j, neight;
-    for(j = 0; j < t[i].degreeOut; ++j){
+    for(j = 0; j < t[i].degreeOut; ++j) {
         neight = t[i].neightOut[j];
-        if(t[neight].color == 0){
+        if(t[neight].color == 0) {
             t[neight].parent = i;
             printf("fael: %d %d\n", i, neight);
             depth(t, neight, time);
         }
-        else{
-            if( ((t[neight].color) == 1) && (t[neight].reach < t[i].reach) ){
+        else {
+            if( ((t[neight].color) == 1) && (t[neight].reach < t[i].reach) ) {
                 printf("Vissza mutato el: %d %d\n", i, neight);
             }
-            else{
-                if(t[neight].color == 2){
-                    if(t[neight].reach > t[i].reach){
+            else {
+                if(t[neight].color == 2) {
+                    if(t[neight].reach > t[i].reach) {
                         printf("Elora mutato el: %d %d\n", i, neight);
                     }
-                    else{
-                        if(t[neight].reach == t[i].reach){
+                    else {
+                        if(t[neight].reach == t[i].reach) {
                             printf("Keresztel: %d %d\n", i, neight);
                         }
                     }
@@ -69,35 +71,34 @@ void depth(Node* t, int i, int time){
     t[i].leave = time;
 }
 
-void DFS(Node* t, int n){
+void DFS(Node* t, int n) {
     int i;
-    for(i = 1; i <= n; ++i){
-        if(t[i].color == 0){
+    for(i = 1; i <= n; ++i) {
+        if(t[i].color == 0) {
             depth(t, i, 0);
         }
     }
 }
 
-int main()
-{
+int main() {
     FILE* f;
-    if(!(f = fopen("pont.txt", "r"))){
+    if(!(f = fopen("pont.txt", "r"))) {
         printf("The file doesn't exist.\n");
         return 1;
     }
-    int i, j, n, m; /// n - csomopontok szama; m - elek szama
+    int i, j, n, m; // n - csomopontok szama; m - elek szama
 
     fscanf(f, "%d", &n);
     fscanf(f, "%d", &m);
     Node* t;
-    if(!(t = (Node*)malloc((n + 1) * sizeof(Node)))){
+    if(!(t = (Node*)malloc((n + 1) * sizeof(Node)))) {
         printf("Allocation failed.\n");
         return 1;
     }
-    for(i = 1; i <= n; ++i){ ///inicializalas
+    for(i = 1; i <= n; ++i) { // inicializalas
         t[i].neightIn = (int*)malloc(n);
         t[i].neightOut = (int*)malloc(n);
-        t[i].color = 0; ///feher
+        t[i].color = 0; // feher
         t[i].degreeIn = 0;
         t[i].degreeOut = 0;
         t[i].parent = 0;
@@ -105,24 +106,24 @@ int main()
         t[i].leave = 0;
     }
     int node1, node2;
-    for(i = 1; i <= m; ++i){
+    for(i = 1; i <= m; ++i) {
         fscanf(f, "%d%d", &node1, &node2);
         t[node1].neightOut[t[node1].degreeOut++] = node2;
         t[node2].neightIn[t[node2].degreeIn++] = node1;
     }
-    for(i = 1; i <= n; ++i){
-        if(!(t[i].neightIn = realloc(t[i].neightIn, (t[i].degreeIn + 1) * sizeof(int)))){
+    for(i = 1; i <= n; ++i) {
+        if(!(t[i].neightIn = realloc(t[i].neightIn, (t[i].degreeIn + 1) * sizeof(int)))) {
             printf("Reallocation failed.\n");
             return 1;
         }
-        if(!(t[i].neightOut = realloc(t[i].neightOut, (t[i].degreeOut + 1) * sizeof(int)))){
+        if(!(t[i].neightOut = realloc(t[i].neightOut, (t[i].degreeOut + 1) * sizeof(int)))) {
             printf("Reallocation failed.\n");
             return 1;
         }
     }
-    ///nodeOut(t, n);
+    // nodeOut(t, n);
     DFS(t, n);
-    ///timeOut(t, n);
+    // timeOut(t, n);
 
     for(i = 0; i <= n; ++i) {
         free(t[i].neightOut);
--- Gráfelmélet/szellesegi(BFS)/main.c
+++ Gráfelmélet/szellesegi(BFS)/main.c
@@ -1,84 +1,84 @@
-///szomszedsagi listaval dolgozunk
+// szomszedsagi listaval dolgozunk
+// kiirja az utat a kezdo csomoponttol a tobbibe
 
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Node{
+typedef struct Node {
     int degree;
-    int* neight; ///a csomopont szomszedjai
-    int color; ///0 = feher(meg nem jartunk ott); 1 = szurke(most vagyunk ott)\
+    int* neight; // a csomopont szomszedjai
+    int color; // 0 = feher(meg nem jartunk ott); 1 = szurke(most vagyunk ott)\
                     2 = fekete(mar jartunk ott)
     int distance;
     int parent;
 }Node;
 
-void nodeOut(Node* t, int n){
+void nodeOut(Node* t, int n) {
     int i, j;
-    for(i = 1; i <= n; ++i){
+    for(i = 1; i <= n; ++i) {
         printf("%d: ", i);
-        for(j = 0; j < t[i].degree; ++j){
+        for(j = 0; j < t[i].degree; ++j) {
             printf("%d ", t[i].neight[j]);
         }
         printf("fokszam: %d\n", t[i].degree);
     }
 }
 
-void parents(Node* t, int n){
+void parents(Node* t, int n) {
     printf("\nParents:\n");
     int i;
-    for(i = 1; i <= n; ++i){
+    for(i = 1; i <= n; ++i) {
         printf("%d %d\n", i, t[i].parent);
     }
 }
 
-void distance(Node* t, int n){
+void distance(Node* t, int n) {
     printf("\nDistance:\n");
     int i;
-    for(i = 1; i <= n; ++i){
+    for(i = 1; i <= n; ++i) {
         printf("%d %d\n", i, t[i].distance);
     }
 }
 
-void route(Node* t, int node){
-    if(t[node].parent != 0){
+void route(Node* t, int node) {
+    if(t[node].parent != 0) {
         route(t, t[node].parent);
     }
     printf("%d ", node);
 }
 
-int main()
-{
+int main() {
     FILE* f;
-    if(!(f = fopen("BFS.txt", "r"))){
+    if(!(f = fopen("BFS.txt", "r"))) {
         printf("The file doesn't exist.\n");
         return 1;
     }
-    int i, j, n, m; /// n - csomopontok szama; m - elek szama
+    int i, j, n, m; // n - csomopontok szama; m - elek szama
 
     fscanf(f, "%d", &n);
     fscanf(f, "%d", &m);
     Node* t;
-    if(!(t = (Node*)malloc((n + 1) * sizeof(Node)))){
+    if(!(t = (Node*)malloc((n + 1) * sizeof(Node)))) {
         printf("Allocation failed.\n");
         return 1;
     }
-    for(i = 1; i <= n; ++i){
+    for(i = 1; i <= n; ++i) {
         t[i].neight = (int*)malloc(n);
-        t[i].color = 0; ///feher
+        t[i].color = 0; // feher
         t[i].degree = 0;
         t[i].distance = 0;
         t[i].parent = 0;
     }
     int node1, node2;
-    for(i = 0; i <= n; ++i){
+    for(i = 0; i <= n; ++i) {
         fscanf(f, "%d%d", &node1, &node2);
         t[node1].neight[t[node1].degree] = node2;
         t[node1].degree++;
         t[node2].neight[t[node2].degree] = node1;
         t[node2].degree++;
     }
-    for(i = 1; i <= n; ++i){
-        if(!(t[i].neight = realloc(t[i].neight, t[i].degree * sizeof(int)))){
+    for(i = 1; i <= n; ++i) {
+        if(!(t[i].neight = realloc(t[i].neight, t[i].degree * sizeof(int)))) {
             printf("Reallocation failed.\n");
             return 1;
         }
@@ -87,33 +87,32 @@ int main()
 
     int* fifo = (int*)calloc(n+1, sizeof(int));
     int first = 0, last = 1, lastNode, firstNode, startNode = 0;
-    printf("\nStarting Node: ");
+    printf("\nKezdo csomopont: ");
     scanf("%d", &startNode);
-    fifo[first] = startNode; ///melyik csomoponttol indulunk ki
+    fifo[first] = startNode; // melyik csomoponttol indulunk ki
 
     t[fifo[first]].color = 1;
-    while(first < last){
-        for(i = 0; i < t[fifo[first]].degree; ++i){
+    while(first < last) {
+        for(i = 0; i < t[fifo[first]].degree; ++i) {
             lastNode = t[fifo[first]].neight[i];
-            if(t[lastNode].color == 0){ ///ha feher, tehat meg nem jartunk ott
-               fifo[last] = lastNode; ///betesszuk a listaba
-               t[lastNode].color = 2; ///feketere allitjuk, mivel ott mar jartunk
+            if(t[lastNode].color == 0) { // ha feher, tehat meg nem jartunk ott
+               fifo[last] = lastNode; // betesszuk a listaba
+               t[lastNode].color = 2; // feketere allitjuk, mivel ott mar jartunk
                t[lastNode].parent = fifo[first];
                t[lastNode].distance = t[fifo[first]].distance + 1;
-               printf("The route to %d: ", lastNode);
+               printf("Az ut %d-ig: ", lastNode);
                route(t, lastNode);
                printf("\n");
                ++last;
             }
         }
         firstNode = fifo[first];
-        printf("%d ", fifo[first]);
         ++first;
     }
 
 
     free(fifo);
-    for(i = 0; i <= n; ++i){
+    for(i = 0; i <= n; ++i) {
         free(t[i].neight);
     }
     free(t);
